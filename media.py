7a24af956bf3e8108c4fabe064e3e84b5b281d58
vnoogueira <43282562+vnoogueira@users.noreply.github.com>
2018-09-14 18:28:12 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+class Movie():
+    def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+        self.title = movie_title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+    
